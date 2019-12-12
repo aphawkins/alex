ba5243c2107c7c5a7a61bf02d83cc1b40de1c961
aphawkins <aph828@gmail.com>
2019-12-12 16:46:48 +0000

    jingle bells

---
+++ microbit/jinlge-bells.py
@@ -0,0 +1,24 @@
+# Add your Python code here. E.g.
+from microbit import *
+
+import music
+music.set_tempo(ticks=4, bpm=120)
+jingle = [
+    'e:2','e:2','e:4',
+    'e:2','e:2','e:4',
+    'e:2','g:2','c','d',
+    'e:8','f:2','f:2','f:3',
+    'f:1','f:2','e:2',
+    'e:2','e:2','e:1',
+    'e:2','d:2','d:2',
+    'e:2','d:4','g:4',
+    'e:2','e:2','e:4',
+    'e:2','e:2','e:4',
+    'e:2','g:2','c','d',
+    'e:8','f:2','f:2','f:2',
+    'f:2','f:2','e:2',
+    'e:2','e:1','e:1',
+    'g:2','g:2','f:2',
+    'd:2','c:4',
+]
+music.play(jingle,wait=False,loop=False)
