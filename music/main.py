d676f1b5a62e9019b58569f9fff1920bcb8aaac1
aphawkins <aph828@gmail.com>
2019-12-12 22:23:57 +0000

    Add silent night and tune choice

--- music/main.py
+++ music/main.py
@@ -1,14 +1,28 @@
 from microbit import *
 import jinglebells
+import silentnight
 import wewishyouamerrychristmas
 import music
-music.set_tempo(ticks=4, bpm=120)
+
+option = 1
+display.show(option)
+
+def play_music(option):
+    if option == 1:
+        musiclib = jinglebells
+    elif option == 2:
+        musiclib = silentnight
+    else:
+        musiclib = wewishyouamerrychristmas
+
+    music.set_tempo(ticks=musiclib.ticks_per_beat, bpm=musiclib.bpm)
+    music.play(musiclib.tune, wait=False, loop=False)
+    display.scroll(musiclib.message)
 
 while True:
-    if button_a.is_pressed():
-        music.play(jinglebells.tune,wait=False,loop=False)
-        display.scroll(jinglebells.message)
-    elif button_b.is_pressed():
-        music.play(wewishyouamerrychristmas.tune,wait=False,loop=False)
-        display.scroll(wewishyouamerrychristmas.message)
-    
+    if button_a.was_pressed():
+        option = option + 1
+        if option > 3: option = 1
+        display.show(option)
+    elif button_b.was_pressed():
+        play_music(option)
